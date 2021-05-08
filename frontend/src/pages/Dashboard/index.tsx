939a79d371d564ba109a2ad21b0b8f8455d86101
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-05-08 04:18:06 -0300

    Update

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -12,7 +12,7 @@ const Dashboard = () => {
           <h1 className="text-primary py-4">Dashboard de vendas</h1>
           <div className="row px-3">
             <div className="col-sm-6">
-              <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+              <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
               <BarChart />
             </div>
             <div className="col-sm-6">
